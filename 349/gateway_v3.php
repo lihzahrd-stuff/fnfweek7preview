9c0b3dd4bdb4ce200b7b8937efd129a909526415
fominizt <82558697+fominizt@users.noreply.github.com>
2021-04-19 13:28:20 +0300

    Add files via upload

---
+++ 349/gateway_v3.php
@@ -0,0 +1,1 @@
+{"success":false,"error":{"message":"Missing required input","code":100},"api_version":"3.0.0","help_url":"http:\/\/www.newgrounds.com\/wiki\/creator-resources\/newgrounds-apis\/newgrounds-io"}
